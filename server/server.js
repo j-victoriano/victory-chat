7573a985079e16ab6aee313c40083ba2753efcb7
Julian Victoriano <jvictoriano35@yahoo.com>
2023-10-06 09:53:27 -0700

    made connection to db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const { chats } = require('./data/data');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("API is available")
@@ -15,4 +18,4 @@ app.get('/api/chat', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`Server Started on Port ${PORT}`));
+app.listen(5000, console.log(`Server Started on Port ${PORT}`.yellow.bold));
