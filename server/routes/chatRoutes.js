dfdc686347792d544c2d0f9c62fdb4ca58347064
Julian Victoriano <jvictoriano35@yahoo.com>
2023-10-08 12:04:19 -0700

    created group chat

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,12 +1,12 @@
 const express= require('express')
-const { accessChat } = require("../controllers/chatControllers")
+const { accessChat, fetchChats, createGroupChat } = require("../controllers/chatControllers")
 const { protect } = require("../middleware/authMiddleware")
 
 const router = express.Router()
 
 router.route('/').post(protect, accessChat)
-// router.route('/').post(protect, fetchChats)
-// router.route('/group').post(protect, createGroupChat)
+router.route('/').get(protect, fetchChats)
+router.route('/group').post(protect, createGroupChat)
 // router.route('/rename').put(protect, renameChat)
 // router.route('/groupremove').put(protect, removeFromChat)
 // router.route('/groupadd').put(protect, addToChat)
