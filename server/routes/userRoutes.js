609e856c09a3eb08e8b619f6ffadd089c34b2362
Julian Victoriano <jvictoriano35@yahoo.com>
2023-10-08 09:52:47 -0700

    added auth mw

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const {registerUser, authUser} = require('../controllers/userControllers')
+const {registerUser, authUser, allUsers} = require('../controllers/userControllers')
+const {authorize} = require('../middleware/authMiddleware')
 const router = express.Router()
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(authorize, allUsers)
 router.post('/login', authUser)
+router.route('/')
 
 module.exports = router
